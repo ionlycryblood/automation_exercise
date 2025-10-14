d3245efdaaf829ffd4461a7d0d013dcc39cfe5b5
timmy <timlill@mail.ru>
2025-10-14 18:26:33 +0300

    xyi

--- test/test_search_product.py
+++ test/test_search_product.py
@@ -12,5 +12,3 @@ def test_searching(page, load, email='timlill@mail.ru', password='qwerty', name_
     products_page.search(name_of_item)
 
     products_page.item_should_be_searched(name_of_item)
-    gfdgfd
-    gfdgfdfd@asfa
